af89911d865a794c96b4ce977165a7db5ef57a21
zakimed1 <154912909+zakimed1@users.noreply.github.com>
2023-12-27 20:00:39 +0100

    Update script.js
    discord link

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function openPage(url) {
 // }, 2250); // 3000 milliseconds (adjust as needed)
 
 function openDiscordInvite(){
-window.open("https://discord.gg/daDJU2CWU6");
+window.open("https://discord.gg/yuYsMAAsBx");
 }window
 
 var d = new Date();
