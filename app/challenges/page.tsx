35dd96a848dc4070e200546df085b71e3995f99a
Moumen.exe <ourmoumen@gmail.com>
2024-03-19 16:50:53 +0100

    add the logiv for registration btn

--- app/challenges/page.tsx
+++ app/challenges/page.tsx
@@ -1,12 +1,63 @@
+"use client";
 import Header from "@/components/Header";
+import { Button } from "@nextui-org/button";
+import { useState } from "react";
+
+function Jionchalnge() {
+  return (
+    <div className="text-right p-7">
+      <p className="pt-8 ">Discord التحدي يقام في تطبيق</p>
+      <p className="pt-3 ">عليك تحميله. تجد فديو إرشادي أسفل الصفحة..</p>
+      <p className="pt-3 ">
+        JOIN-TFC تدخل إليها، ثم بعد قراءتك للوصف تضغط على إستمارة بدأ
+      </p>
+      <p className="pt-3 ">
+        التحدي حتى يتم تسجيلك في المشاركين ثم إضغط على -إبدأ التحدي- والذي يأخذك
+        إلى غرفتك الخاصة لإجراء التحدي. أي ستقوم بكتابة اسمك الكامل وعناونك
+        البريدي email
+      </p>
+      <p className="pt-3 ">
+        ويبدأ عد الأيام - يمكنك بدأ التحدي متى شئت - بتطبيقك لقواعد التحدي يترتب
+        عليك مشاركة تقدمك في إنجازها كمشاركة اليومية مع إنتهاء كل نهار. وكذا
+        إرسالك رسالة يومية قبل 5 صباحا لإثبات حضورك. بعد إنهائك التحدي تقوم بملء
+        إستمارة تجدها في غرفة join-tfc# حتى تعلم المسؤول عن إنتهائك ويتم مراجعة
+        نتائجك. طريقة تقييم النتائج تختلف من مشارك لآخر. المهم أن على كل شخص
+        تقديم كل ما لديه. وفي حالة فشلك يطلب منك إعادة التحدي إذا كنت حقا لاتزال
+        راغبا في تغيير حياتك. لا توجد أي تساهلات في تقييم التحدي وداخل المجموعة.
+        بعد أن يتم قبولك في المجموعة سيعرض عليك شرح مفصل عن المجموعة الخاصة وكذا
+        القواعد العامة لها حتى تستطيع فهم كيفية سير الأمور.
+      </p>
+      <div className=" flex justify-center items-center">
+        <p className="pt-8 font-bold text-xl font-sans  ">
+          One DAY or Day One !
+        </p>
+      </div>
+
+      <div className=" flex justify-center items-center">
+        <button className=" mt-2 p-2 rounded-md flex pv-1 items-center justify-center  border-solid border-1 border-[#473c89]  bg-[#473c89] focus:bg-[#584bac] font-bold font-mono hover:bg-transparent ">
+          <a href="https://discord.gg/gWmNstdYnj" target="_blank">
+            {" "}
+            Discord
+          </a>
+        </button>
+      </div>
+
+      <p></p>
+    </div>
+  );
+}
 
 export default function Challenges() {
+  const [Challenge, setchalenge] = useState(false);
+  function clickhandler() {
+    setchalenge(true);
+  }
   return (
-    <div className="h-screen w-full max-sm:h-full">
+    <div className="h-full w-full max-sm:h-full">
       <Header />
       <div className="flex flex-col justify-center items-center p-7">
         <div className=" mt-10">
-          <h1 className="text-2xl font-bold text-center text-white">
+          <h1 className="text-3xl m-4 font-bold text-center text-white">
             Algeria 2024
           </h1>
           <p className="text-lg text-center text-white">
@@ -35,21 +86,22 @@ export default function Challenges() {
                 <p className="text-xs text-right text-white">{e.content}</p>
               </div>
             ))}
-            <div></div>
+
             <div className="h-full flex justify-center items-center">
               <div className="w-full text-center py-3 rounded-[20px] border-2 border-[#fec401]">
-                <a
-                  href="#"
+                <Button
+                  onClick={clickhandler}
                   className="text-xl font-bold text-center text-[#f2f2f2]"
                 >
                   {" "}
                   التسجيل هنا
-                </a>
+                </Button>
               </div>
             </div>
           </div>
         </div>
       </div>
+      {Challenge && <Jionchalnge />}
     </div>
   );
 }
