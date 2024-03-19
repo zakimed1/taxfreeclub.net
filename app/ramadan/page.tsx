132b688df566b3d0eb415ab44996722d774ae5a1
Jervi Sir <gacembekhira@gmail.com>
2024-03-19 23:19:15 +0100

    added ramadan folder, but as coming soon

---
+++ app/ramadan/page.tsx
@@ -0,0 +1,7 @@
+export default function Ramadan() {
+  return (
+    <div style={{height: '63vh', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+      <h1>Coming Soon.</h1>
+    </div>
+  )
+}
