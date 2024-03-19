8a74ad6417dce39f4830549f32f317a079740bdb
Moumen.exe <ourmoumen@gmail.com>
2024-03-19 16:25:02 +0100

    responsive about

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -6,12 +6,12 @@ import Header from "@/components/Header";
 export default function Component() {
   return (
     // replace header with nav bar compo
-    <div className="bg-black text-white w-full   h-screen">
+    <div className="bg-black text-white w-full  h-full max-md:h-full max-sm:h-full">
       <Header />
-      <main className="w-screen py-16 px-8 max-sm:py-4 max-sm:px-2  ">
+      <main className="w-full h-screen mt-10 max-md:h-full max-sm:h-full  px-8 max-sm:py-4 max-sm:px-2  ">
         <section className="space-y-8 flex w-11/12 text  max-sm:text-xs max-sm:flex-col">
           <div className="flex items-center justify-between">
-            <Image src={Logo24}  alt="logo" />
+            <Image src={Logo24} alt="logo" />
           </div>
           <article className="space-y-4 text-right w-full">
             <h2 className="text-xl font-semibold">:TFC تعريف</h2>
@@ -21,13 +21,13 @@ export default function Component() {
               الجماعي. مهمتنا واحدة: تحقيق الحرية المالية من خلال الأعمال
               الداخلية والوصول لنمط حياة غني ومتطور
             </p>
-            <h2 className="text-xl font-semibold">مهمتنا:</h2>
+            <h2 className="text-xl font-semibold">:مهمتنا</h2>
             <p>
               العزلة عدو كل قبطان يريد تطوير حياته، فإن هذا النادي لأن يوفر له
               الجو الجماعي الملائم ليتعاون مع من هم بنفس عقليته على التركيز في
               طريق العمل وتحقيق أهدافهم معًا مع طريق تبادل الخبرات والمنافع
             </p>
-            <h2 className="text-xl font-semibold">القواعد والقيم :</h2>
+            <h2 className="text-xl font-semibold">:القواعد والقيم</h2>
             <p>
               يجب على كل الأعضاء إحترام القوانين العامة للمجموعة أهمها القبطان
               يدعم خوه:على كل فرد دعم زميله في حالة إحتياجه له حسب إستطاعته
@@ -35,13 +35,13 @@ export default function Component() {
               مهما كانت أقدميته أنفع قبل ما تنتفع:على كل فرد أن ينضم بنية تبادل
               النفع وليس بالإستغلال فقط. إضافة قيمة للمجموعة إجباري
             </p>
-            <h2 className="text-xl font-semibold">تقييم الأداء :</h2>
+            <h2 className="text-xl font-semibold">:تقييم الأداء </h2>
             <p>
               يتم تقييم أداء الأعضاء بحسب إلتزامهم بقواعد وقيم المجموعة، ويكون
               هذا بشكل أسبوعي. في حال عدم تحسن أداء فرد بعد تدهوره. يتم إقصاؤه
               ويتوجب عليه إعادة التحدي من جديد
             </p>
-            <h2 className="text-xl font-semibold">الفرص المقدمة :</h2>
+            <h2 className="text-xl font-semibold">:الفرص المقدمة</h2>
             <p>
               يتم تقديم فرص للأعضاء الذين يثبتون أنفسهم في التعلم والعمل ومساعدة
               باقي الفريق بما يستطيعون تقديمه. وهذا حتى يواصلوا طريقهم فيما
