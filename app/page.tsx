91448508542707ea08134d0d629986cd34352990
Jervi Sir <gacembekhira@gmail.com>
2024-03-19 23:16:24 +0100

    added animations

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+'use client'
 import Image from "next/image";
 import Header from "@/components/Header";
 import { Footer } from "@/components/footer";
@@ -5,15 +6,23 @@ import Hero from "./home/page";
 import { Section2 } from "@/app/home/Section2";
 import { Section3 } from "@/app/home/Section3";
 import { Section4 } from "./home/Section4";
-import { Fade } from 'react-awesome-reveal';
+import { Fade, Zoom } from 'react-awesome-reveal';
 
 export default function Home() {
   return (
     <>
+    <Fade cascade damping={0.1} triggerOnce>
       <Hero/>
+    </Fade>
+    <Fade cascade damping={0.1} triggerOnce>
       <Section2 />
+    </Fade>
+    <Fade cascade damping={0.1} triggerOnce>
       <Section3 />
+    </Fade>
+    <Fade cascade damping={0.1} triggerOnce>
       <Section4 />
+    </Fade>
     </>
   );
 }
